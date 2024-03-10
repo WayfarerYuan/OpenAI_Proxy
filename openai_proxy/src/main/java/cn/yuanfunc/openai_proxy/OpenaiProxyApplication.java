55abfa9345b9648f6d5f486b34b5b72b7beff6cc
Wayfarer Yuan <voyageryuan@outlook.com>
2024-03-10 19:21:43 +0800

    20240310

---
+++ openai_proxy/src/main/java/cn/yuanfunc/openai_proxy/OpenaiProxyApplication.java
@@ -0,0 +1,13 @@
+package cn.yuanfunc.openai_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiProxyApplication.class, args);
+	}
+
+}
